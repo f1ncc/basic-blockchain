f6e127de4ab690b1b3f7b8fee0393c103a4632a9
sathya717 <rajavslk@gmail.com>
2020-03-12 23:08:11 +0530

    added few features-1

--- src/blockchain.js
+++ src/blockchain.js
@@ -9,10 +9,20 @@ class Block{
         this.data = data;
         this.prevHash = prevHash;
         this.hash = this.calcHash();
+        this.nonce = 0;
     }
     calcHash()
     {
-        return SHA256(this.index + this.timestamp + this.prevHash + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.timestamp + this.prevHash + JSON.stringify(this.data) + this.nonce).toString();
+    }
+    mineBlock(difficulty)
+    {
+        while(this.hash.substring(0,difficulty) !== Array(difficulty+1).join("0"))
+        {
+            this.nonce++;
+            this.hash = this.calcHash();
+        }
+        console.log(`Block is mined-> Hash-> ${this.hash}`)
     }
 }
 
