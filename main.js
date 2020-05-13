b2c7e6113f48a71e018a835c9958e1c2a559dafe
Sathya LK <rajavslk@gmail.com>
2020-05-13 21:11:28 +0530

    prettier code 2.0

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-import { Blockchain, Transaction } from './src/blockchain';
+import { Blockchain, Transaction } from "./src/blockchain";
 
-const EC = require('elliptic').ec
-const ec = new EC('secp256k1')
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
 
-const myPrivKey = ec.keyFromPrivate('49076404c1f63338df8d7d97a9114c1425866d7857b61e1c776f15b8469b3d4f')
-const walletAddress = myPrivKey.getPublic('hex')
+const myPrivKey = ec.keyFromPrivate(
+  "49076404c1f63338df8d7d97a9114c1425866d7857b61e1c776f15b8469b3d4f"
+);
+const walletAddress = myPrivKey.getPublic("hex");
 
 var d = new Date();
 var newCoin = new Blockchain();
-const firstTrans = new Transaction(walletAddress, 'put public key', 96)
-firstTrans.signTransaction(myPrivKey)
-newCoin.addTransaction(firstTrans)
+const firstTrans = new Transaction(walletAddress, "put public key", 96);
+firstTrans.signTransaction(myPrivKey);
+newCoin.addTransaction(firstTrans);
 
-console.log('\n Starting miner->')
-newCoin.minePendTransactions(walletAddress)
+console.log("\n Starting miner->");
+newCoin.minePendTransactions(walletAddress);
 
-console.log(`\n Balance is ${newCoin.getBalance(walletAddress)}`)
+console.log(`\n Balance is ${newCoin.getBalance(walletAddress)}`);
 
-module.exports = d
+module.exports = d;
--- src/keygen.js
+++ src/keygen.js
@@ -1,10 +1,10 @@
-const EC = require('elliptic').ec
-const ec = new EC('secp256k1')
+const EC = require("elliptic").ec;
+const ec = new EC("secp256k1");
 
 const key = ec.genKeyPair();
-const pubkey = key.getPublic('hex')
-const privkey = key.getPrivate('hex')
+const pubkey = key.getPublic("hex");
+const privkey = key.getPrivate("hex");
 
-console.log(pubkey)
-console.log()
-console.log(privkey)
+console.log(pubkey);
+console.log();
+console.log(privkey);
